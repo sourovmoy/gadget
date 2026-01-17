efed12d9bf2607b8d46f8395648dee4c2823ecb7
sourovmmoysanju <sourovmmoysanju@gmail.com>
2026-01-17 14:04:44 +0600

    test 6

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -17,6 +17,11 @@ const nextConfig = {
     ],
     unoptimized: true,
   },
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/job-task-assignment' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/job-task-assignment/' : '',
 }
 
 export default nextConfig
